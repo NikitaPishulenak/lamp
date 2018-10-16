cf3f2c520924cc8eef0b2a2b536524bf8a34d6c1
Mikita <niki1995-11@mail.ru>
2018-10-16 21:57:23 +0300

    Хочу сделать появление фото при прокрутке

--- scr/script.js
+++ scr/script.js
@@ -8,4 +8,18 @@ $(document).ready(function(){
 
 	$("#cDate").text(cDate.toLocaleString("ru", options));
 	$("a.s_b_photo").fancybox();
+	// $("div").click(function(e){
+	// 	console.log($(this).parent().parent().position().top);
+	// });
+	$(document).scroll(function() {
+	  $('.hidden').each(function(e) {
+	  	console.log(e.pageY);
+	    if ((Number(e.pageY)-Number($(this).parent().parent().position().top)-Number($(this).position().top))<=0) {
+	    	//console.log('1');
+	      $(this).animate({
+	        opacity: 1
+	      }, 2000);
+	    }
+	  });
+	});
 });
