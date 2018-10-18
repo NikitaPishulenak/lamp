47b9cc9949794ed311ca9007e0bb4f53f6603227
BSMU\PishulenakNA <niki1995-11@mail.ru>
2018-10-18 14:54:32 +0300

    Начал делать для малых разрешений

--- scr/script.js
+++ scr/script.js
@@ -6,17 +6,26 @@ $(document).ready(function(){
 	  timezone: 'UTC',
 	};
 
-	$("#cDate").text(cDate.toLocaleString("ru", options));
-	$("a.s_b_photo").fancybox();
-	// $("div").click(function(e){
-	// 	console.log($(this).parent().parent().position().top);
-	// });
+	$(".cDate").text(cDate.toLocaleString("ru", options));
+
+	$("a[rel=img_group]").fancybox({
+		'transitionIn'		: 'none',
+		'transitionOut'		: 'none',
+		'titlePosition' 	: 'over',
+		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
+			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
+		}
+	});
+
 	$(document).scroll(function() {
 	  $('.hidden').each(function() {
 	  	if($(this).css("opacity")!=1){
-	  		if (($(this).position().top+$(this).parent().parent().parent().position().top-$(document).scrollTop())<700) {
+	  		if (($(this).position().top+$(this).parent().parent().parent().position().top-$(document).scrollTop())<850) {
 		      $(this).animate({
 		        opacity: 1
+		      }, 2000);
+		      $(this).parent().animate({
+		        marginTop: '50px'
 		      }, 2000);
 		    }
 	  	}
